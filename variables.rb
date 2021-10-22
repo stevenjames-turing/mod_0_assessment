3d60ab6cc2de95f6c9623bd94a6cb3215d531a9c
Steven James <StevenJamesSTL@gmail.com>
2021-10-22 12:14:30 -0500

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,19 @@
+famous_person = "Nicolas Cage"
+
+is_active = true
+
+famous_person = "Bill Murray"
+
+num_3 = 3
+num_9 = 9
+
+sum = num_3 + num_9
+
+animals = ["lion", "tiger", "bear"]
+
+# removes first element from array
+animals.shift
+puts animals
+
+# prints character count for a string
+puts famous_person.length
