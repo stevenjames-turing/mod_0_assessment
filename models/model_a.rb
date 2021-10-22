3f5ffd7a0e7c76a1e7264476355e6d7ba832abec
Steven James <StevenJamesSTL@gmail.com>
2021-10-22 12:22:30 -0500

    Add model_a

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+international_airport = {
+  airport_name: "Denver International Airport",
+  airport_city: "Denver",
+  terminal_count: 4,
+  avg_flights_per_day: 212.7,
+  efficient_security: false,
+  smart_location: false,
+  airline_companies: ["Delta", "Southwest", "United", "Frontier"],
+  shop_types: ["restaurant", "souvenir", "sunglasses", "books"]
+}
