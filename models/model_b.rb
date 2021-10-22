edd36b839d492596f2037d0b0088b2923ff0ea81
Steven James <StevenJamesSTL@gmail.com>
2021-10-22 12:37:59 -0500

    Add model_b

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+current_vehicle = {
+  manufacturer: "Ford",
+  model: "F-150",
+  engine_size_liters: 2.7,
+  seating_capacity: 5,
+  is_scratched: true,
+  is_reliable: true,
+  storage_compartments: ["truck bed", "glove box", "under-seat", "center console"],
+  states_driven: ["Colorado", "Utah", "Nevada", "Kansas", "Missouri", "New Mexico", "Wyoming", "Nebraska", "Iowa", "Illinios"]
+}
